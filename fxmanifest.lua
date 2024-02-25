7f0b12fd4006b13f3aaf7653b7be3fd77ba50870
GitHub-Pural <129020547+GitHub-Pural@users.noreply.github.com>
2024-02-25 15:50:57 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Pural'
+description 'This is a radial Menu based on ox_lib with a functional clothing menu'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+	'config.lua',
+}
+
+client_scripts {
+	'clothing.lua',
+}
